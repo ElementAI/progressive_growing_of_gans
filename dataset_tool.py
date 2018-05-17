09f2c6fe97d1475a77a86528f49bd69fc3f3c9a8
tboquet <thomas.boquet@hec.ca>
2018-05-17 16:29:42 -0400

    + command

--- dataset_tool.py
+++ dataset_tool.py
@@ -700,6 +700,11 @@ def execute_cmdline(argv):
     p.add_argument(     'tfrecord_dir',     help='New dataset directory to be created')
     p.add_argument(     'cifar10_dir',      help='Directory containing CIFAR-10')
 
+    p = add_command(    'create_ssense',   'Create dataset for SSENSE',
+                                            'create_cifar10 datasets/SSENSE/downloads/SSENSE')
+    p.add_argument(     'tfrecord_dir',     help='New dataset directory to be created')
+    p.add_argument(     'cifar10_dir',      help='Directory containing SSENSE')
+
     p = add_command(    'create_cifar100',  'Create dataset for CIFAR-100.',
                                             'create_cifar100 datasets/cifar100 ~/downloads/cifar100')
     p.add_argument(     'tfrecord_dir',     help='New dataset directory to be created')
