a4fe94314b60ddc102a1340d329ae7855fa56be9
Boris Oreshkin <boris@elementai.com>
2018-07-04 17:03:59 -0400

    Added generator network using FILM layers

--- networks.py
+++ networks.py
@@ -19,14 +19,14 @@ def cset(cur_lambda, new_cond, new_lambda): return lambda: tf.cond(new_cond, new
 #----------------------------------------------------------------------------
 # Get/create weight tensor for a convolutional or fully-connected layer.
 
-def get_weight(shape, gain=np.sqrt(2), use_wscale=False, fan_in=None):
+def get_weight(shape, gain=np.sqrt(2), use_wscale=False, fan_in=None, name='weight'):
     if fan_in is None: fan_in = np.prod(shape[:-1])
     std = gain / np.sqrt(fan_in) # He init
     if use_wscale:
         wscale = tf.constant(np.float32(std), name='wscale')
-        return tf.get_variable('weight', shape=shape, initializer=tf.initializers.random_normal()) * wscale
+        return tf.get_variable(name, shape=shape, initializer=tf.initializers.random_normal()) * wscale
     else:
-        return tf.get_variable('weight', shape=shape, initializer=tf.initializers.random_normal(0, std))
+        return tf.get_variable(name, shape=shape, initializer=tf.initializers.random_normal(0, std))
 
 #----------------------------------------------------------------------------
 # Fully-connected layer.
@@ -46,6 +46,46 @@ def conv2d(x, fmaps, kernel, gain=np.sqrt(2), use_wscale=False):
     w = get_weight([kernel, kernel, x.shape[1].value, fmaps], gain=gain, use_wscale=use_wscale)
     w = tf.cast(w, x.dtype)
     return tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME', data_format='NCHW')
+
+
+#----------------------------------------------------------------------------
+# FILM Layer postmultipliers
+
+def get_film_postmultiplier(weight_decay_film):
+    beta_postmultiplier = tf.get_variable(name='beta_postmultiplier', dtype=tf.float32, initializer=0.0,
+                                  regularizer=tf.contrib.layers.l2_regularizer(scale=weight_decay_film,
+                                                                               scope='penalize_beta'))
+    gamma_postmultiplier = tf.get_variable(name='gamma_postmultiplier', dtype=tf.float32, initializer=0.0,
+                                   regularizer=tf.contrib.layers.l2_regularizer(scale=weight_decay_film,
+                                                                                scope='penalize_gamma'))
+    tf.summary.scalar('beta_postmultiplier', beta_postmultiplier)
+    tf.summary.scalar('gamma_postmultiplier', gamma_postmultiplier)
+    return beta_postmultiplier, gamma_postmultiplier
+
+
+#----------------------------------------------------------------------------
+# FILM Layer
+
+def apply_film(x, text_embed, weight_decay_film, **kwargs):
+    embed_size = text_embed.shape.as_list()[1]
+    fmaps = x.shape.as_list()[1]
+    with tf.variable_scope('FILM'):
+        gamma_weight = get_weight(shape=[embed_size, fmaps], use_wscale=False, name='gamma_weight')
+        beta_weight = get_weight(shape=[embed_size, fmaps], use_wscale=False, name='gamma_weight')
+
+        beta_postmultiplier, gamma_postmultiplier = get_film_postmultiplier(weight_decay_film)
+
+        gamma = tf.matmul(text_embed, gamma_weight)
+        beta = tf.matmul(text_embed, beta_weight)
+
+        beta = tf.multiply(beta_postmultiplier, beta, name='postmultiply_beta')
+        gamma = 1.0 + tf.multiply(gamma_postmultiplier, gamma, name='postmultiply_gamma')
+
+        if len(x.shape) == 2:
+            return x*gamma + beta
+        else:
+            return x*tf.reshape(gamma, [-1, fmaps, 1, 1]) + tf.reshape(beta, [-1, fmaps, 1, 1])
+
 
 #----------------------------------------------------------------------------
 # Apply bias to the given activation tensor.
@@ -137,6 +177,116 @@ def minibatch_stddev_layer(x, group_size=4):
         y = tf.cast(y, x.dtype)                                 # [M111]  Cast back to original data type.
         y = tf.tile(y, [group_size, 1, s[2], s[3]])             # [N1HW]  Replicate over group and pixels.
         return tf.concat([x, y], axis=1)                        # [NCHW]  Append as new fmap.
+
+
+# ----------------------------------------------------------------------------
+# Generator network with FILM.
+
+def G_film(
+        latents_in,  # First input: Latent vectors [minibatch, latent_size].
+        labels_in,  # Second input: Labels [minibatch, label_size].
+        num_channels=1,  # Number of output color channels. Overridden based on dataset.
+        resolution=32,  # Output resolution. Overridden based on dataset.
+        label_size=0,  # Dimensionality of the labels, 0 if no labels. Overridden based on dataset.
+        fmap_base=8192,  # Overall multiplier for the number of feature maps.
+        fmap_decay=1.0,  # log2 feature map reduction when doubling the resolution.
+        fmap_max=512,  # Maximum number of feature maps in any layer.
+        latent_size=None,  # Dimensionality of the latent vectors. None = min(fmap_base, fmap_max).
+        normalize_latents=True,  # Normalize latent vectors before feeding them to the network?
+        use_wscale=True,  # Enable equalized learning rate?
+        use_pixelnorm=True,  # Enable pixelwise feature vector normalization?
+        pixelnorm_epsilon=1e-8,  # Constant epsilon for pixelwise feature vector normalization.
+        use_leakyrelu=True,  # True = leaky ReLU, False = ReLU.
+        dtype='float32',  # Data type to use for activations and outputs.
+        fused_scale=True,  # True = use fused upscale2d + conv2d, False = separate upscale2d layers.
+        structure=None,  # 'linear' = human-readable, 'recursive' = efficient, None = select automatically.
+        is_template_graph=False,  # True = template graph constructed by the Network class, False = actual evaluation.
+        **kwargs):  # Ignore unrecognized keyword args.
+
+    resolution_log2 = int(np.log2(resolution))
+    assert resolution == 2 ** resolution_log2 and resolution >= 4
+
+    def nf(stage):
+        return min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
+
+    def PN(x):
+        return pixel_norm(x, epsilon=pixelnorm_epsilon) if use_pixelnorm else x
+
+    if latent_size is None: latent_size = nf(0)
+    if structure is None: structure = 'linear' if is_template_graph else 'recursive'
+    act = leaky_relu if use_leakyrelu else tf.nn.relu
+
+    latents_in.set_shape([None, latent_size])
+    labels_in.set_shape([None, label_size])
+    combo_in = tf.cast(tf.concat([latents_in, labels_in], axis=1), dtype)
+    lod_in = tf.cast(tf.get_variable('lod', initializer=np.float32(0.0), trainable=False), dtype)
+
+    # Building blocks.
+    def block(x, res, text_embed, weight_decay_film):  # res = 2..resolution_log2
+        with tf.variable_scope('%dx%d' % (2 ** res, 2 ** res)):
+            if res == 2:  # 4x4
+                if normalize_latents: x = pixel_norm(x, epsilon=pixelnorm_epsilon)
+                with tf.variable_scope('Dense'):
+                    x = dense(x, fmaps=nf(res - 1) * 16, gain=np.sqrt(2) / 4,
+                              use_wscale=use_wscale)  # override gain to match the original Theano implementation
+                    x = tf.reshape(x, [-1, nf(res - 1), 4, 4])
+                    x = apply_film(x, text_embed, weight_decay_film)
+                    x = PN(act(x))
+                with tf.variable_scope('Conv'):
+                    x = conv2d(x, fmaps=nf(res - 1), kernel=3, use_wscale=use_wscale)
+                    x = apply_film(x, text_embed, weight_decay_film)
+                    x = PN(act(x))
+            else:  # 8x8 and up
+                if fused_scale:
+                    with tf.variable_scope('Conv0_up'):
+                        x = upscale2d_conv2d(x, fmaps=nf(res - 1), kernel=3, use_wscale=use_wscale)
+                        x = apply_film(x, text_embed, weight_decay_film)
+                        x = PN(act(x))
+                else:
+                    x = upscale2d(x)
+                    with tf.variable_scope('Conv0'):
+                        x = conv2d(x, fmaps=nf(res - 1), kernel=3, use_wscale=use_wscale)
+                        x = apply_film(x, text_embed, weight_decay_film)
+                        x = PN(act(x))
+                with tf.variable_scope('Conv1'):
+                    x = conv2d(x, fmaps=nf(res - 1), kernel=3, use_wscale=use_wscale)
+                    x = apply_film(x, text_embed, weight_decay_film)
+                    x = PN(act(x))
+            return x
+
+    def torgb(x, res):  # res = 2..resolution_log2
+        lod = resolution_log2 - res
+        with tf.variable_scope('ToRGB_lod%d' % lod):
+            return apply_bias(conv2d(x, fmaps=num_channels, kernel=1, gain=1, use_wscale=use_wscale))
+
+    # Linear structure: simple but inefficient.
+    if structure == 'linear':
+        x = block(combo_in, 2, text_embed=combo_in, **kwargs)
+        images_out = torgb(x, 2)
+        for res in range(3, resolution_log2 + 1):
+            lod = resolution_log2 - res
+            x = block(x, res, text_embed=combo_in, **kwargs)
+            img = torgb(x, res)
+            images_out = upscale2d(images_out)
+            with tf.variable_scope('Grow_lod%d' % lod):
+                images_out = lerp_clip(img, images_out, lod_in - lod)
+
+    # Recursive structure: complex but efficient.
+    if structure == 'recursive':
+        def grow(x, res, lod):
+            y = block(x, res)
+            img = lambda: upscale2d(torgb(y, res), 2 ** lod)
+            if res > 2: img = cset(img, (lod_in > lod),
+                                   lambda: upscale2d(lerp(torgb(y, res), upscale2d(torgb(x, res - 1)), lod_in - lod),
+                                                     2 ** lod))
+            if lod > 0: img = cset(img, (lod_in < lod), lambda: grow(y, res + 1, lod - 1))
+            return img()
+
+        images_out = grow(combo_in, 2, resolution_log2 - 2)
+
+    assert images_out.dtype == tf.as_dtype(dtype)
+    images_out = tf.identity(images_out, name='images_out')
+    return images_out
 
 #----------------------------------------------------------------------------
 # Generator network used in the paper.
