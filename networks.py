8e0ca3c4635f7ee97ac5c81504bd55591f3d80eb
tboquet <thomas.boquet@hec.ca>
2018-10-10 13:25:21 -0400

    * tf.constant

--- networks.py
+++ networks.py
@@ -376,7 +376,7 @@ def attention(x, ch, scope='attention'):
 
 def hw_flatten(x):
     x_shape = x.get_shape().as_list()
-    shape_tensor = tf.convert_to_tensor([x_shape[0], -1, x_shape[-1]])
+    shape_tensor = tf.convert_to_tensor(tf.constant([x_shape[0], -1, x_shape[-1]]))
     return tf.reshape(x, shape=shape_tensor)
 
 
